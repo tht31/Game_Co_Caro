b6d14cd8f31e7d25e200d00a3557bc644e14993b
tht31 <24020328@vnu.edu.vn>
2025-03-24 21:55:09 +0700

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,11 @@
+#ifndef _DEFS__H
+#define _DEFS__H
+
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 750;
+const char* WINDOW_TITLE = "Caro Clash";
+#define BOARD_X 10
+#define BOARD_Y 10
+#define CELL_SIZE 30
+
+#endif
