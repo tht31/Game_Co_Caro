69ea5b6d484914917cbd1d927446bbe2be23a42c
tht31 <24020328@vnu.edu.vn>
2025-03-28 21:17:52 +0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -2,7 +2,7 @@
 #define _DEFS__H
 
 const int SCREEN_WIDTH = 600;
-const int SCREEN_HEIGHT = 6000;
+const int SCREEN_HEIGHT = 600;
 const char* WINDOW_TITLE = "Caro Clash";
 #define BOARD_X 10
 #define BOARD_Y 10
